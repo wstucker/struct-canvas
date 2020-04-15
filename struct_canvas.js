b6b805e34d4935b74d08310d211fcbec1774c1fb
William Stucker <william.stucker@lightelligence.ai>
2020-04-14 21:50:58 -0400

    Cleanup stuff

--- struct_canvas.js
+++ struct_canvas.js
@@ -1,6 +1,21 @@
 
-class Struct {
+class StructConfig {
+    constructor() {
+        // Reset the canvas width to maximum visible width
+        this.resize_to_window_width = true;
+        // Size of MSB and LSB numbering font above struct bar in pixels
+        this.xsb_font_height = 10;
+        // Height of struct bar in pixels
+        this.bar_height = 30;
+        // Height in pixels of gap between two consecutive structs
+        this.bar_spacer_height = 5;
+        // Size of font of field names in pixels
+        this.field_name_font_height = 14;
+    }
+}
 
+
+class Struct {
     /**
      * canvas: element
      * data: list of dictionaries
@@ -10,37 +25,76 @@ class Struct {
                 lsbs
              All are lists
     */
-    constructor(canvas, data) {
+    constructor(canvas, data, config) {
         this.canvas = canvas;
         this.data = data;
+        this.config = config;
 
-        this.render_struct(data[0]);
-    }
+        if (this.config.resize_to_window_width) {
+            // There's probably a better way to do this
+            this.canvas.width = window.innerWidth * 0.99;
+        }
 
-    render_struct(struct) {
-        //var c = document.getElementById("myCanvas");
-        this.canvas.width = window.innerWidth * 0.99;
-        var pixels_per_bit = this.canvas.width / (Math.max.apply(null, struct["msbs"]) + 1);
+        this.last_rendered_y = 0;
+        this.max_msb = 0; // calculated in check_data
 
-        var bit_rows_required = Math.ceil(Math.log10(Math.max.apply(null, struct["msbs"])));
+        this.check_data();
 
-        var struct_top = 0;
-
-        var bit_font_height = 10;
-
-        var bar_top = struct_top + bit_font_height * bit_rows_required;
-        var bar_height = 30;
-
-        var field_font_height = 14;
-        var field_font_top = (bar_height / 2) + bar_top;
+        this.pixels_per_bit = this.canvas.width / (this.max_msb + 1);
+        this.bit_rows_required = Math.ceil(Math.log10(this.max_msb));
+        
+        for (var i=0; i < data.length ; i++) {
+            this.render_struct(data[i]);
+        }
 
         // Readjust canvas height to make sure it fits
         // May not want to do this if this is just a function that gets applied to the same canvas
-        this.canvas.height = bar_top + bar_height;
+        this.canvas.height = this.last_rendered_y;
+
+        // Need to rerender because the resize right above wipes everythign out
+        // FIXME, maybe in the previous render, can skip render and just to calculations
+        this.last_rendered_y = 0;
+        for (var i=0; i < data.length ; i++) {
+            this.render_struct(data[i]);
+        }
+    }
+
+    error(msg) {
+        // FIMXE bettter way to render these errors?
+        window.alert(msg)
+    }
+
+    check_data() {
+        // Also calculates max_msb
+        if (this.data.lenth == 0) {
+            this.error("data was empty");
+        }
+        var msb_set = new Set()
+        for (var i=0; i < this.data.length; i++) {
+            var struct = this.data[i];
+            if ((struct["field_names"].length != struct["msbs"].length) ||
+                (struct["field_names"].length != struct["lsbs"].length)) {
+                this.error("data[" + i + "] had inconsistent lengths");
+            }
+            msb_set.add(Math.max.apply(null, struct["msbs"]));
+        }
+        if (msb_set.size != 1) {
+            this.error("Each element in the data list should have the same maximum msb for prettiest alignment. Use seperate Struct objects for different width structs.");
+        }
+        for (var it = msb_set.values(), val= null; val=it.next().value; ) {
+            this.max_msb = val;
+            break;
+        }
+    }
+
+    render_struct(struct) {
+        var bar_top = this.last_rendered_y + this.config.xsb_font_height * this.bit_rows_required;
+
+        var field_font_top = (this.config.bar_height / 2) + bar_top;
 
         var ctx = this.canvas.getContext("2d");
         ctx.beginPath();
-        ctx.rect(0, bar_top, this.canvas.width, bar_height);
+        ctx.rect(0, bar_top, this.canvas.width, this.config.bar_height);
         ctx.closePath();
         ctx.stroke();
 
@@ -48,18 +102,16 @@ class Struct {
         var required_padding = ctx.measureText("  ").width
 
         for (var i = 0; i < struct["field_names"].length; i++) {
-
             var txt = struct["field_names"][i];
             var actual_txt_width = ctx.measureText(txt).width + required_padding;
             var bit_width = struct["msbs"][i] - struct["lsbs"][i] + 1;
-            var allowed_txt_width = pixels_per_bit * bit_width;
+            var allowed_txt_width = this.pixels_per_bit * bit_width;
 
             if (actual_txt_width > allowed_txt_width) {
                 txt = "*";
+                // FIXME need to make a legend at the bottom?
             }
 
-
-            //var box_width = actual_txt_width;
             var box_width = allowed_txt_width;
             var x_start = last_box_x;
             var x_stop = x_start + box_width;
@@ -67,16 +119,16 @@ class Struct {
             last_box_x = x_stop;
 
             ctx.beginPath();
-            ctx.font = field_font_height.toString().concat("px Arial");
+            ctx.font = this.config.field_name_font_height.toString().concat("px Arial");
             ctx.textAlign = "center";
             ctx.textBaseline = "middle";
-            ctx.rect(x_start + 1, bar_top + 1, box_width - 2, bar_height - 2);
+            ctx.rect(x_start + 1, bar_top + 1, box_width - 2, this.config.bar_height - 2);
             ctx.fillText(txt, x_center, field_font_top);
             ctx.closePath();
             ctx.stroke();
 
             var bit_row_bottom = bar_top;
-            for (var bit_row = 0; bit_row < bit_rows_required; bit_row++) {
+            for (var bit_row = 0; bit_row < this.bit_rows_required; bit_row++) {
 
                 var msb_text_align = "left";
                 var msb_x = x_start + 2;
@@ -90,7 +142,7 @@ class Struct {
                 if (msb_str.length >= bit_row + 1) {
                     var msb_digit = msb_str.charAt(msb_str.length - bit_row - 1)
                     ctx.beginPath();
-                    ctx.font = bit_font_height.toString().concat("px Arial");
+                    ctx.font = this.config.xsb_font_height.toString().concat("px Arial");
                     ctx.textAlign = msb_text_align;
                     ctx.textBaseline = "bottom";
                     ctx.fillText(msb_digit, msb_x, bit_row_bottom);
@@ -104,7 +156,7 @@ class Struct {
                     if (lsb_str.length >= bit_row + 1) {
                         var lsb_digit = lsb_str.charAt(lsb_str.length - bit_row - 1)
                         ctx.beginPath();
-                        ctx.font = bit_font_height.toString().concat("px Arial");
+                        ctx.font = this.config.xsb_font_height.toString().concat("px Arial");
                         ctx.textAlign = "right";
                         ctx.textBaseline = "bottom";
                         ctx.fillText(lsb_digit, x_stop - 2, bit_row_bottom);
@@ -112,10 +164,11 @@ class Struct {
                         ctx.stroke();
                     }
                 }
-                bit_row_bottom = bit_row_bottom - bit_font_height;
+                bit_row_bottom = bit_row_bottom - this.config.xsb_font_height;
 
             }
 
         }
+        this.last_rendered_y = bar_top + this.config.bar_height + this.config.bar_spacer_height;
     }
 }
