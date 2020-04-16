5b438cf579288b2c9dacaf4fea82dbd3043695f9
William Stucker <william.stucker@lightelligence.ai>
2020-04-15 22:36:14 -0400

    Allow labels for multibar

--- struct_canvas.js
+++ struct_canvas.js
@@ -20,14 +20,15 @@ class Struct {
     /**
      * canvas: element
      * data: list of dictionaries
-             dictionary must have three keys:
-                field_names
-                msbs
-                lsbs
-             All are lists
+             dictionary keys:
+                field_names (mandatory) list of strings
+                msbs        (mandatory) list of ints
+                lsbs        (mandatory) list of ints
+                label       (optional)  string: a brief label tot show up on left of bar
     */
     constructor(canvas, data, config) {
         this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
         this.data = data;
         this.config = config;
 
@@ -38,10 +39,11 @@ class Struct {
 
         this.last_rendered_y = 0;
         this.max_msb = 0; // calculated in check_data
-
+        this.label_width_px = 0;
+        
         this.check_data();
 
-        this.pixels_per_bit = this.canvas.width / (this.max_msb + 1);
+        this.pixels_per_bit = (this.canvas.width - this.label_width_px) / (this.max_msb + 1);
         this.bit_rows_required = Math.ceil(Math.log10(this.max_msb));
         
         for (var i=0; i < data.length ; i++) {
@@ -70,7 +72,9 @@ class Struct {
         if (this.data.lenth == 0) {
             this.error("data was empty");
         }
-        var msb_set = new Set()
+        var msb_set = new Set();
+        var label_widths = new Array();
+        this.ctx.font = this.config.field_name_font_height.toString().concat("px Arial");
         for (var i=0; i < this.data.length; i++) {
             var struct = this.data[i];
             if ((struct["field_names"].length != struct["msbs"].length) ||
@@ -78,7 +82,14 @@ class Struct {
                 this.error("data[" + i + "] had inconsistent lengths");
             }
             msb_set.add(Math.max.apply(null, struct["msbs"]));
+            if ("label" in struct) {
+                label_widths.push(this.ctx.measureText(" " + struct["label"] + " ").width);
+            }
         }
+        if (label_widths.length) {
+            this.label_width_px = Math.max.apply(null, label_widths);
+        }
+
         if (msb_set.size != 1) {
             this.error("Each element in the data list should have the same maximum msb for prettiest alignment. Use seperate Struct objects for different width structs.");
         }
@@ -93,18 +104,27 @@ class Struct {
 
         var field_font_top = (this.config.bar_height / 2) + bar_top;
 
-        var ctx = this.canvas.getContext("2d");
-        ctx.beginPath();
-        ctx.rect(0, bar_top, this.canvas.width, this.config.bar_height);
-        ctx.closePath();
-        ctx.stroke();
+        this.ctx.beginPath();
+        this.ctx.rect(this.label_width_px, bar_top, this.canvas.width, this.config.bar_height);
+        if ("label" in struct) {
+            this.ctx.font = this.config.field_name_font_height.toString().concat("px Arial");
+            this.ctx.textAlign = "right";
+            this.ctx.textBaseline = "middle";
+            this.ctx.fillText(struct["label"] + " ", this.label_width_px, field_font_top);
+        }
+        this.ctx.closePath();
+        this.ctx.stroke();
 
-        var last_box_x = 1;
-        var required_padding = ctx.measureText("  ").width
+        var last_box_x = this.label_width_px + 1;
+        if ("offset_msb" in struct) {
+            last_box_x = struct["offset_msb"] * this.pixels_per_bit;
+        }
+        
+        var required_padding = this.ctx.measureText("  ").width
 
         for (var i = 0; i < struct["field_names"].length; i++) {
             var txt = struct["field_names"][i];
-            var actual_txt_width = ctx.measureText(txt).width + required_padding;
+            var actual_txt_width = this.ctx.measureText(txt).width + required_padding;
             var bit_width = struct["msbs"][i] - struct["lsbs"][i] + 1;
             var allowed_txt_width = this.pixels_per_bit * bit_width;
 
@@ -119,19 +139,21 @@ class Struct {
             var x_center = x_start + box_width / 2;
             last_box_x = x_stop;
 
-            ctx.beginPath();
-            ctx.font = this.config.field_name_font_height.toString().concat("px Arial");
-            ctx.textAlign = "center";
-            ctx.textBaseline = "middle";
-            ctx.rect(x_start + 1, bar_top + 1, box_width - 2, this.config.bar_height - 2);
-            ctx.fillText(txt, x_center, field_font_top);
-            ctx.closePath();
-            ctx.stroke();
+            this.ctx.beginPath();
+            this.ctx.font = this.config.field_name_font_height.toString().concat("px Arial");
+            this.ctx.textAlign = "center";
+            this.ctx.textBaseline = "middle";
+            if (txt != "") {
+                this.ctx.rect(x_start + 1, bar_top + 1, box_width - 2, this.config.bar_height - 2);
+            }
+            this.ctx.fillText(txt, x_center, field_font_top);
+            this.ctx.closePath();
+            this.ctx.stroke();
 
-            ctx.beginPath();
-            ctx.font = this.config.xsb_font_height.toString().concat("px Arial");
-            ctx.textAlign = "center";
-            ctx.textBaseline = "bottom";
+            this.ctx.beginPath();
+            this.ctx.font = this.config.xsb_font_height.toString().concat("px Arial");
+            this.ctx.textAlign = "center";
+            this.ctx.textBaseline = "bottom";
             var bit_row_bottom = bar_top;
             for (var bit_row = 0; bit_row < this.bit_rows_required; bit_row++) {
 
@@ -145,7 +167,7 @@ class Struct {
                 var msb_str = struct["msbs"][i].toString();
                 if (msb_str.length >= bit_row + 1) {
                     var msb_digit = msb_str.charAt(msb_str.length - bit_row - 1)
-                    ctx.fillText(msb_digit, msb_x, bit_row_bottom);
+                    this.ctx.fillText(msb_digit, msb_x, bit_row_bottom);
                 }
 
                 if (struct["msbs"][i] != struct["lsbs"][i]) {
@@ -153,14 +175,14 @@ class Struct {
                     var lsb_str = struct["lsbs"][i].toString();
                     if (lsb_str.length >= bit_row + 1) {
                         var lsb_digit = lsb_str.charAt(lsb_str.length - bit_row - 1)
-                        ctx.fillText(lsb_digit, x_stop - (this.pixels_per_bit / 2), bit_row_bottom);
+                        this.ctx.fillText(lsb_digit, x_stop - (this.pixels_per_bit / 2), bit_row_bottom);
                     }
                 }
                 bit_row_bottom = bit_row_bottom - this.config.xsb_font_height;
 
             }
-            ctx.closePath();
-            ctx.stroke();
+            this.ctx.closePath();
+            this.ctx.stroke();
 
         }
         this.last_rendered_y = bar_top + this.config.bar_height + this.config.bar_spacer_height;
